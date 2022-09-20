d9e6b8ae7d52cac84824a39e61c39e421eccd6e5
Cody Butz <codyjbutz@gmail.com>
2022-09-20 15:57:06 -0400

    Minting Policy with Native Scripts issue

--- src/Scaffold.purs
+++ src/Scaffold.purs
@@ -2,10 +2,52 @@ module Scaffold (contract) where
 
 import Contract.Prelude
 
-import Contract.Address as Contract.Address
-import Contract.Log as Contract.Log
-import Contract.Monad (Contract)
+import Contract.Chain (currentSlot)
+import Contract.Hashing (nativeScriptHash)
+import Contract.Monad (Contract, liftedE, liftedM, liftContractM)
+import Contract.Prim.ByteArray (byteArrayFromAscii)
+import Contract.ScriptLookups as Lookups
+import Contract.Scripts (NativeScript(..))
+import Contract.Time (Slot (..))
+import Contract.Transaction (awaitTxConfirmed, balanceAndSignTx, submit)
+import Contract.TxConstraints as Constraints
+import Contract.Value (TokenName, mkTokenName)
 
+import Data.BigInt as BigInt
+
+import Types.BigNum as BigNum
+import NativeScripts (NativeScriptHash(..))
+import Plutus.Types.CurrencySymbol (scriptHashAsCurrencySymbol, currencyMPSHash)
+
+-- This contract fails since the MPS Hash is not a Plutus Script. The `mustMintCurrency`
+-- requires that a plutus script be included as a lookup, however it's possible to use
+-- Native Scripts as a minting policy.
 contract :: Contract () Unit
-contract = Contract.Log.logInfo' <<< show =<<
-  Contract.Address.ownPaymentPubKeyHash
+contract = do
+  slot <- currentSlot
+  let newSlot = pure $ BigNum.add (unwrap slot) (BigNum.fromInt 180)
+  newSlot' <- liftedM "Unable to calculate new slot" newSlot
+  tn <- tokenName "TheToken"
+  let script = ScriptAll [
+    TimelockExpiry (Slot newSlot')
+  ]
+  (NativeScriptHash scriptHash) <- liftedM "Unable to create native script" $ pure $ nativeScriptHash script
+  let cs = scriptHashAsCurrencySymbol scriptHash
+  let mph = currencyMPSHash cs
+
+  let
+    lookups :: Lookups.ScriptLookups Void
+    lookups = mempty
+  let
+    constraints :: Constraints.TxConstraints Void Void
+    constraints = Constraints.mustMintCurrency mph tn (BigInt.fromInt 1)
+
+  ubTx <- liftedE $ Lookups.mkUnbalancedTx lookups constraints
+  bsTx <- liftedM "Failed to balance/sign tx" $ balanceAndSignTx ubTx
+  txId <- submit bsTx
+  awaitTxConfirmed txId
+
+tokenName :: String -> Contract () TokenName
+tokenName =
+  liftContractM "Cannot make token name"
+    <<< (mkTokenName <=< byteArrayFromAscii)
